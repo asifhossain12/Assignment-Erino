d57dd4662375c15051029c5f107bdde43350e177
Shaikh Asif Hossain <168845892+asifhossain12@users.noreply.github.com>
2024-11-16 23:44:52 +0530

    Update serializers.py

--- contact_project/contact_app/serializers.py
+++ contact_project/contact_app/serializers.py
@@ -7,13 +7,13 @@ class ContactSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_email(self, value):
-        # Check if it's a new instance
+       
         if self.instance is None:
-            # If it's a new contact, check if email already exists
+           
             if Contact.objects.filter(email=value).exists():
                 raise serializers.ValidationError("This email is already in use.")
         else:
-            # If updating, exclude the current instance by its ID
+          
             if Contact.objects.filter(email=value).exclude(id=self.instance.id).exists():
                 raise serializers.ValidationError("This email is already in use.")
 
