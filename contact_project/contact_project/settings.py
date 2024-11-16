8602f595d309bc3cd9cae0d7634ae4f2fc1974f9
Shaikh Asif Hossain <168845892+asifhossain12@users.noreply.github.com>
2024-11-17 00:21:20 +0530

    Update settings.py

--- contact_project/contact_project/settings.py
+++ contact_project/contact_project/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 
